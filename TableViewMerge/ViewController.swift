f90bf1ad9c9c2ce237d5a2d5f92b539a42b96ceb
Anton Pomozov <pomozoff@mail.ru>
2017-03-06 19:17:34 +0300

    Initial Commit

---
+++ TableViewMerge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewMerge
+//
+//  Created by Anton Pomozov on 06/03/2017.
+//  Copyright © 2017 Anton Pomozov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
